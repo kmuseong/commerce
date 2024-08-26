2da991df7490c7c2ffad556fa932a7fda7d7b834
kim-museong <antjd0419@gmail.com>
2024-08-27 03:01:30 +0900

    FEATURE : 판매자 상품추가페이지 라우트 설정

--- src/app/provider/Router.tsx
+++ src/app/provider/Router.tsx
@@ -4,14 +4,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { LoginPage } from '@/pages/LoginPage';
 import { SignupPage } from '@/pages/SignupPage';
 import { HomePage } from '@/pages/HomePage';
+import { CreateProductPage } from '@/pages/CreateProductPage';
 
 const Router: React.FC = () => {
     return (
         <BrowserRouter>
             <Routes>
+                {/** 유저페이지 */}
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/signup" element={<SignupPage />} />
+
+                {/** 판매자페이지 */}
+                <Route path="/create" element={<CreateProductPage />} />
             </Routes>
         </BrowserRouter>
     );
