d4c9713da3babd4a9fd32e92e88e10d3dc3a232b
kim-museong <antjd0419@gmail.com>
2024-08-30 19:42:22 +0900

    feature : 이미지 업로드 기능 및 디테일 페이지 이미지 적용

--- src/features/productDetail/api/api.ts
+++ src/features/productDetail/api/api.ts
@@ -2,7 +2,18 @@ import supabase from '@/supabaseClient';
 
 export const getProduct = async (id: string) => {
     try {
-        const { data, error } = await supabase.from('products').select('*').eq('id', id);
+        const { data, error } = await supabase
+            .from('products')
+            .select(
+                `
+            *,
+            product_images (
+                id,
+                image_url
+            )
+        `
+            )
+            .eq('id', id);
 
         if (error) console.log({ error });
 
