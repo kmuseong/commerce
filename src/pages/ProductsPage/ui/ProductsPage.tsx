336404c2e2bea6f5bd5cdcd5d4e31dec2021cf0a
kim-museong <antjd0419@gmail.com>
2024-09-06 00:54:12 +0900

    cartIcon 적용

--- src/pages/ProductsPage/ui/ProductsPage.tsx
+++ src/pages/ProductsPage/ui/ProductsPage.tsx
@@ -1,21 +1,19 @@
 import { Products } from '@/features/products';
 import { Input } from '@/shared/components/ui/input';
 import { Header } from '@/widgets/header/ui/Header';
-import { Search, ShoppingBag } from 'lucide-react';
+import { Search } from 'lucide-react';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import classes from './ProductsPage.module.css';
 import { ProductFiter } from '@/features/productFiter';
+import { CartIcon } from '@/widgets/cartIcon';
 
 export const ProductsPage: React.FC = () => {
-    const navigate = useNavigate();
-
     return (
         <div>
             <Header>
                 <div className={classes.headerNav}>
                     <div>로고</div>
-                    <ShoppingBag onClick={() => navigate(`/cart`)} />
+                    <CartIcon />
                 </div>
                 <div className={classes.search}>
                     <Input />
