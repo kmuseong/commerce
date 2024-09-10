0c316466daed71cb0c4a34f4a0bbf1f6eb3dd062
kim-museong <antjd0419@gmail.com>
2024-09-10 12:41:40 +0900

    comment : 사용하지 않는 import코드 주석처리

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+// import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
-import compression from 'vite-plugin-compression';
+// import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
