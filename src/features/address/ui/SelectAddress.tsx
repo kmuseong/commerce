895d90a8e6a09967c6e7e66188a8b6f3cf7e51c9
kim-museong <antjd0419@gmail.com>
2024-09-12 20:54:18 +0900

    FEATURE : 선택된 배송지화면 컴포넌트

---
+++ src/features/address/ui/SelectAddress.tsx
@@ -0,0 +1,51 @@
+import { getSelectAddress } from '@/features/address/api/api';
+import { Button } from '@/shared/components/ui/button';
+import { useAuthStore } from '@/shared/stores/auth/useAuthStore';
+import { useOrderStore } from '@/shared/stores/order/useOrderStore';
+import { Address } from '@/widgets/address';
+import { useQuery } from '@tanstack/react-query';
+import React, { useEffect } from 'react';
+
+export const SelectAddress: React.FC = () => {
+    const { user } = useAuthStore();
+    const { address, changeAddress } = useOrderStore();
+
+    const { data, isLoading } = useQuery({
+        queryKey: ['selectAddress'],
+        queryFn: () => getSelectAddress(user?.id as string),
+    });
+
+    const openAddressWindow = () => {
+        const newWindow = window.open(
+            `/my/address?select=${address?.id}`,
+            '주소설정',
+            'width=600,height=800,left=100,top=100'
+        );
+
+        if (newWindow) {
+            const timer = setInterval(() => {
+                if (newWindow.closed) {
+                    clearInterval(timer);
+                }
+            }, 500);
+        }
+    };
+
+    useEffect(() => {
+        changeAddress(data!);
+    }, [changeAddress, data]);
+
+    if (isLoading) {
+        return <div>로딩중..</div>;
+    }
+
+    return (
+        <div className="flex flex-col gap-2 p-4">
+            <Address item={address || data!} />
+
+            <Button onClick={openAddressWindow} variant="outline">
+                주소설정
+            </Button>
+        </div>
+    );
+};
